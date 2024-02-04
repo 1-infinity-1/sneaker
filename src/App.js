ab09825524e2e8a36beb7283d119cc6b69657c73
Eduard <dealsoka@gmail.com>
2024-02-04 19:58:09 +0700

    layout header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.scss';
 
 function App() {
-  return (
-      <div>Hello world</div>
-  );
+    return (
+        <div className="wrapper">
+            <header>
+                <div className="headerLeft">
+                    <img width={40} height={40} src="/image/logo.png"/>
+                    <div className="headerInfo">
+                        <h3>React Sneakers</h3>
+                        <p>Магазин лучших кросовок</p>
+                    </div>
+                </div>
+                <div className="headerRight">
+                    <div>
+                        <img width={18} height={18} src="/image/cartIcon.png"/>
+                        <span>1205 руб.</span>
+                    </div>
+                    <div>
+                        <img width={18} height={18} src="/image/likeIcon.png"/>
+                        <span>Закладки</span>
+                    </div>
+                    <div>
+                        <img width={18} height={18} src="/image/profileIcon.png"/>
+                        <span>Профиль</span>
+                    </div>
+                </div>
+            </header>
+            <div className="content">
+                <h1>Все кроссовки</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
